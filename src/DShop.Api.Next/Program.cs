5bc1f0e5ac57746586054ef4040b469f07b93780
Piotr Gankiewicz <spetz90@gmail.com>
2019-01-22 17:50:33 +0100

    Updated to Ntrada

--- src/DShop.Api.Next/Program.cs
+++ src/DShop.Api.Next/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using NGate;
+using Ntrada;
 
 namespace DShop.Api.Next
 {
     public static class Program
     {
         public static async Task Main(string[] args)
-            => await WebHost.CreateDefaultBuilder(args).UseNGate().Build().RunAsync();
+            => await WebHost.CreateDefaultBuilder(args).UseNtrada().Build().RunAsync();
     }
 }
